ec4a1ad9b4d12acadfd60062de29d357c5d186ed
Francis Giraldeau <francis.giraldeau@gmail.com>
2015-03-09 13:45:01 -0400

    Add stride operation
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- ext/crunch.c
+++ ext/crunch.c
@@ -2,16 +2,37 @@
 #include <bytearrayobject.h>
 
 PyObject*
-do_crunch(PyObject* self, PyObject* args)
+do_stride(PyObject* self, PyObject* args)
 {
     (void) self, (void) args;
-    printf("do_crunch %p %p\n", self, args);
+    PyByteArrayObject *src_obj, *dst_obj;
+    char *src, *dst;
+    Py_ssize_t src_size, dst_size;
+    int stride, height, i, j, x, y;
+
+    if (!PyArg_ParseTuple(args, "YYi", &dst_obj, &src_obj, &stride)) {
+        Py_RETURN_NONE;
+    }
+
+    src = PyByteArray_AS_STRING(src_obj);
+    dst = PyByteArray_AS_STRING(dst_obj);
+    src_size = PyByteArray_Size((PyObject *)src_obj);
+    dst_size = PyByteArray_Size((PyObject *)dst_obj);
+    height = src_size / stride;
+
+    for (i = 0; i < stride; i++) {
+        for (j = 0; j < height; j++) {
+            x = (i * height + j) % dst_size;
+            y = (i + j * stride) % src_size;
+            dst[x] = src[y];
+        }
+    }
     Py_RETURN_NONE;
 }
 
 static PyMethodDef ProfileBenchMethods[] =
 {
-    {"crunch",       do_crunch,     METH_VARARGS, NULL},
+    {"stride",       do_stride,     METH_VARARGS, NULL},
     {NULL, NULL, 0, NULL}
 };
 
