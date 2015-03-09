2f22dde8f1915282df61b16103d201cd2781f8de
Francis Giraldeau <francis.giraldeau@gmail.com>
2015-03-09 13:44:46 -0400

    Code formating
    Signed-off-by: Francis Giraldeau <francis.giraldeau@gmail.com>

--- perfuserBench/accuracy.py
+++ perfuserBench/accuracy.py
@@ -4,6 +4,7 @@ Created on Mar 5, 2015
 @author: francis
 '''
 from linuxProfile import api, utils
+from perfuserBench import ext
 import time
 import pandas as pd
 import numpy as np
@@ -43,33 +44,33 @@ def f_08(fn, *args, **kwargs):
 def f_09(fn, *args, **kwargs):
     fn(*args, **kwargs)
 
-def work(size = 0):
+def work(size=0):
     data = list(range(size))
     data.reverse()
     data.sort()
 
 class ProgressBar(object):
-    def __init__(self, total_work = 1, width = 40):
+    def __init__(self, total_work=1, width=40):
         self._total_work = float(total_work)
         self._width = width
     def update(self, current):
         bar = int(math.ceil((current / self._total_work) * self._width))
-        space = self._width - bar 
+        space = self._width - bar
         sys.stdout.write("[{}{}]\r".format("#" * bar, " " * space))
         sys.stdout.flush()
     def done(self):
         sys.stdout.write("\ndone\n")
-        sys.stdout.flush() 
+        sys.stdout.flush()
 
 def check_linear(fn):
     data = { 'size': [], 'time': [] }
     max_power = 20
     max_repeat = 10
-    bar = ProgressBar(total_work = max_power * max_repeat)
+    bar = ProgressBar(total_work=max_power * max_repeat)
     work = 0
     for _ in range(max_repeat):
         for power in range(max_power):
-            size = 2**power
+            size = 2 ** power
             bar.update(work)
             t1 = time.clock()
             fn(size)
@@ -84,17 +85,17 @@ def check_linear(fn):
     res = grp['time'].aggregate([np.mean, np.std]).reset_index()
     res['iter_ns'] = res['mean'] / res['size'] * 1000000000
     print(res)
-    #r = res['mean'].corr(res['size'])
-    #model = pd.ols(y=res['mean'], x=res['size'])
-    #print(model.summary)
-    #print(model.sm_ols.outlier_test())
-    #print("correlation size v.s. mean: {}".format(r))
-    #p = res.plot(x='size', y='mean')
-    #p.set_yscale('log')
-    #p.set_xscale('log')
-    #plt.show()
+    # r = res['mean'].corr(res['size'])
+    # model = pd.ols(y=res['mean'], x=res['size'])
+    # print(model.summary)
+    # print(model.sm_ols.outlier_test())
+    # print("correlation size v.s. mean: {}".format(r))
+    # p = res.plot(x='size', y='mean')
+    # p.set_yscale('log')
+    # p.set_xscale('log')
+    # plt.show()
 
-def do_experiment(repeat, factors, chunk = 128):
+def do_experiment(repeat, factors, chunk=128):
     for _ in range(repeat):
         for fn, param in factors:
             fn(work, param * chunk)
@@ -113,8 +114,8 @@ def do_linuxprofile(repeat, factors, chunk):
     do_experiment(repeat, factors, chunk)
     api.disable_perf()
 
-def do_crunch():
+def do_stride():
+    data = bytearray([1, 2, 3])
     api.enable_perf()
-    from perfuserBench import ext
-    ext.crunch(bytearray([1,2,3]))
+    ext.stride(data)
     api.disable_perf()
